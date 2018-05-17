9babc258dcce9bbb6578ccf3320871f410314b16
Tyrzal <la163284@student.helha.be>
2018-05-17 22:07:46 +0200

    ok

---
+++ research.php
@@ -0,0 +1,17 @@
+<?php
+	include "admin.php";
+	
+	/*$req = $bdd -> prepare("SELECT * FROM rents WHERE isLocated = false");
+	
+	try
+	{
+		$req -> execute();
+	} catch (Exception $e){}
+	
+	include "navbar.php";*/
+?>
+
+<html>
+	<body>
+	</body>
+</html>
