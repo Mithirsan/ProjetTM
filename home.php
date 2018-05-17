9438af77ff657ff7af9e835346987bc53a986084
Kevin Nobles <nobles-k@hotmail.com>
2018-05-17 22:52:11 +0200

    css push

--- home.php
+++ home.php
@@ -3,28 +3,8 @@
 ?>
 
 
-<div id="homeCarousel" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-        <li data-target="#homeCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#homeCarousel" data-slide-to="1"></li>
-        <li data-target="#homeCarousel" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner embed-responsive embed-responsive-21by9">
-        <div class="carousel-item embed-responsive-item active " style="background-image:url('ressources/images/windwaker.png')">
-        </div>
-        <div class="carousel-item embed-responsive-item" style="background-image:url('ressources/images/majoras.png')">
-        </div>
-        <div class="carousel-item embed-responsive-item" style="background-image:url('ressources/images/afrosamurai.jpg')">
-        </div>
-    </div>
-    <a class="carousel-control-prev" href="#homeCarousel" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#homeCarousel" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
+<div class="container-fluid" >
+    <img class="img-fluid" id="homeimg">
 </div>
 
 <div class="container" id="popular">
@@ -40,27 +20,27 @@
         </div>
         <div class="col-lg-4 col-md-6 col-xs-12" >
             <a href="#" class="d-block mb-4">
-                <img class="img-fluid rounded" src="ressources/images/popularcity/paris.jpg" alt="">
+                <img class="img-fluid rounded" src="ressources/images/popularcity/losangeles.jpg" alt="">
             </a>
         </div>
         <div class="col-lg-4 col-md-6 col-xs-12" >
             <a href="#" class="d-block mb-4">
-                <img class="img-fluid rounded" src="ressources/images/popularcity/paris.jpg" alt="">
+                <img class="img-fluid rounded" src="ressources/images/popularcity/budapest.jpg" alt="">
             </a>
         </div>
         <div class="col-lg-4 col-md-6 col-xs-12" >
             <a href="#" class="d-block mb-4">
-                <img class="img-fluid rounded" src="ressources/images/popularcity/paris.jpg" alt="">
+                <img class="img-fluid rounded" src="ressources/images/popularcity/prague.jpg" alt="">
             </a>
         </div>
         <div class="col-lg-4 col-md-6 col-xs-12" >
             <a href="#" class="d-block mb-4">
-                <img class="img-fluid rounded" src="ressources/images/popularcity/paris.jpg" alt="">
+                <img class="img-fluid rounded" src="ressources/images/popularcity/santorini.jpg" alt="">
             </a>
         </div>
         <div class="col-lg-4 col-md-6 col-xs-12" >
             <a href="#" class="d-block mb-4">
-                <img class="img-fluid rounded" src="ressources/images/popularcity/paris.jpg" alt="">
+                <img class="img-fluid rounded" src="ressources/images/popularcity/amsterdam.jpg" alt="">
             </a>
         </div>
     </div>
