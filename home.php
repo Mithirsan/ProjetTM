359cc8871874f5596e557bc8e0443f9092b6e1d6
Kevin Nobles <nobles-k@hotmail.com>
2018-05-17 23:32:40 +0200

    texte en dessous destination

--- home.php
+++ home.php
@@ -1,46 +1,76 @@
 <?php include "navbar.php"; 
-	
+
 ?>
 
 
 <div class="container-fluid" >
-    <img class="img-fluid" id="homeimg">
+    <img class="img-fluid w-100" src="ressources/images/lasvegas.jpg">
 </div>
 
 <div class="container" id="popular">
-      <hr class="hr-text" data-content="Suggested Destinations">
+    <hr class="hr-text" data-content="Suggested Destinations">
 </div>
 
 <div class="container">
     <div class="row text-center text-lg-left" >
         <div class="col-lg-4 col-md-6 col-xs-12" >
             <a href="#" class="d-block mb-4">
-                <img class="img-fluid rounded" src="ressources/images/popularcity/paris.jpg" alt="">
+                <figure class="figure">
+                    <img class="figure-img img-fluid rounded" src="ressources/images/popularcity/paris.jpg" alt="">
+                    <figcaption class="figure-caption">
+                        <p class="caption" > Paris </p>
+                    </figcaption>
+                </figure>
+            </a>
+        </div>
+        <div class="col-lg-4 col-md-6 col-xs-12" >
+            <a href="#" class="d-block mb-4"> 
+                <figure class="figure">
+                    <img class="figure-img img-fluid rounded" src="ressources/images/popularcity/losangeles.jpg" alt="">
+                    <figcaption class="figure-caption">
+                        <p class="caption" > Los Angeles </p>
+                    </figcaption>
+                </figure>
             </a>
         </div>
         <div class="col-lg-4 col-md-6 col-xs-12" >
             <a href="#" class="d-block mb-4">
-                <img class="img-fluid rounded" src="ressources/images/popularcity/losangeles.jpg" alt="">
+                <figure class="figure">
+                    <img class="figure-img img-fluid rounded" src="ressources/images/popularcity/budapest.jpg" alt="">
+                    <figcaption class="figure-caption">
+                        <p class="caption" > Budapest </p>
+                    </figcaption>
+                </figure>
             </a>
         </div>
         <div class="col-lg-4 col-md-6 col-xs-12" >
             <a href="#" class="d-block mb-4">
-                <img class="img-fluid rounded" src="ressources/images/popularcity/budapest.jpg" alt="">
+                <figure class="figure">
+                <img class="figure-img img-fluid rounded" src="ressources/images/popularcity/prague.jpg" alt="">
+                <figcaption class="figure-caption">
+                        <p class="caption" > Prague </p>
+                    </figcaption>
+                </figure>
             </a>
         </div>
         <div class="col-lg-4 col-md-6 col-xs-12" >
             <a href="#" class="d-block mb-4">
-                <img class="img-fluid rounded" src="ressources/images/popularcity/prague.jpg" alt="">
+                <figure class="figure">
+                <img class="figure-img img-fluid rounded" src="ressources/images/popularcity/santorini.jpg" alt="">
+                    <figcaption class="figure-caption">
+                        <p class="caption" > San Torino </p>
+                    </figcaption>
+                </figure>
             </a>
         </div>
         <div class="col-lg-4 col-md-6 col-xs-12" >
             <a href="#" class="d-block mb-4">
-                <img class="img-fluid rounded" src="ressources/images/popularcity/santorini.jpg" alt="">
-            </a>
-        </div>
-        <div class="col-lg-4 col-md-6 col-xs-12" >
-            <a href="#" class="d-block mb-4">
-                <img class="img-fluid rounded" src="ressources/images/popularcity/amsterdam.jpg" alt="">
+                <figure class="figure">
+                <img class="figure-img img-fluid rounded" src="ressources/images/popularcity/amsterdam.jpg" alt="">
+                <figcaption class="figure-caption">
+                        <p class="caption" > Amsterdam </p>
+                    </figcaption>
+                </figure>
             </a>
         </div>
     </div>
